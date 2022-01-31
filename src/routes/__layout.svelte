7b6c62767365b63ce5edc64d41183c842543b1ed
Shinichi Okada <okada.shin@gmail.com>
2022-02-01 08:09:17 +0900

    Taken out flowbite from svelte:head and added it to body.

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -3,12 +3,10 @@
   import { Navbar } from "@codewithshin/svelte-flow";
 </script>
 
-<svelte:head>
-  <link
-    rel="stylesheet"
-    href="https://unpkg.com/@themesberg/flowbite@1.3.0/dist/flowbite.min.css"
-  />
-</svelte:head>
+<link
+  rel="stylesheet"
+  href="https://unpkg.com/@themesberg/flowbite@1.3.0/dist/flowbite.min.css"
+/>
 
 <div class="mx-auto p-2">
   <Navbar textsize="lg" />
--- src/routes/cards/__layout.svelte
+++ src/routes/cards/__layout.svelte
@@ -1,7 +1,7 @@
-<svelte:head>
-  <link
-    rel="stylesheet"
-    href="https://unpkg.com/@themesberg/flowbite@1.3.0/dist/flowbite.min.css"
-  />
-</svelte:head>
-<slot />
+<link
+  rel="stylesheet"
+  href="https://unpkg.com/@themesberg/flowbite@1.3.0/dist/flowbite.min.css"
+/>
+<div class="mx-auto p-2">
+  <slot />
+</div>
