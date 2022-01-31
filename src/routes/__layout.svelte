dd19398829c0662136841ea78e35ccdbb7784ca7
Shinichi Okada <okada.shin@gmail.com>
2022-02-01 06:55:16 +0900

    Frontend layout updates.

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,49 @@
+<script>
+  import "../app.css";
+  import { Navbar } from "@codewithshin/svelte-flow";
+
+  let menus = [
+    {
+      name: "Home",
+      link: "/",
+    },
+    {
+      name: "Buttons",
+      link: "/buttons",
+    },
+    {
+      name: "Cards",
+      link: "/cards",
+    },
+    {
+      name: "List Group",
+      link: "/list-group",
+    },
+    {
+      name: "Modals",
+      link: "/modals",
+    },
+    {
+      name: "Navbar",
+      link: "/navbar",
+    },
+    {
+      name: "Tabs",
+      link: "/tabs",
+    },
+  ];
+  let logoSrc = "/images/mkdir-logo.png";
+</script>
+
+<svelte:head>
+  <link
+    rel="stylesheet"
+    href="https://unpkg.com/@themesberg/flowbite@1.3.0/dist/flowbite.min.css"
+  />
+</svelte:head>
+
+<div class="container mx-auto px-8">
+  <Navbar {menus} />
+
+  <slot />
+</div>
--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,20 @@
-import vercel from '@sveltejs/adapter-vercel';
+import preprocess from "svelte-preprocess";
+import vercel from "@sveltejs/adapter-vercel";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: vercel(),
+  kit: {
+    adapter: vercel(),
 
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
-	}
+    // hydrate the <div id="svelte"> element in src/app.html
+    target: "#svelte",
+  },
+
+  preprocess: [
+    preprocess({
+      postcss: true,
+    }),
+  ],
 };
 
 export default config;
